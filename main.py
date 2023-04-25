d7e7efce6eda858792e97f95d915aee6fcd7ba67
William Tayeb <tayebwilliam@gmail.com>
2023-04-26 01:37:58 +0200

    Verify operator precedence

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from sys import argv
 from lark import Lark
 
 grammar = open("grammar.lark")
 parser = Lark(grammar, parser='lalr', start='definition')
+
+def p(src):
+  print(parser.parse(src).pretty())
+
+if __name__ == "__main__":
+  src = argv[1]
+  p(src)
