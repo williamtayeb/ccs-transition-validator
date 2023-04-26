c7d6f2a52fb0e0adbefc1d4acde3fd45df09af12
William Tayeb <tayebwilliam@gmail.com>
2023-04-26 17:23:32 +0200

    Ignore pycache folder in vscode

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sys import argv
-from lark import Lark
+from lark import Lark, ParseError, GrammarError
+import inspect
 
 grammar = open("grammar.lark")
 parser = Lark(grammar, parser='lalr', start='definition')
@@ -10,3 +11,12 @@ def p(src):
 if __name__ == "__main__":
   src = argv[1]
   p(src)
+
+# except ParseError as parse_error:
+  # print("Error while parsing grammar")
+# except GrammarError as grammar_error:
+  # print("Grammar error")
+# except Exception as e:
+  # print(e.__class__)
+  # print(e.__class__.__base__)
+  # print(inspect.getmro(type(e)))
